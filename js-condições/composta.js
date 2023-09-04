394a0f92a41001c76c5779d02efb6dd9f99ff565
mkorrea <mathscorrea@gmail.com>
2023-09-04 17:09:51 -0300

    exercicio de condições

---
+++ js-condições/composta.js
@@ -0,0 +1,7 @@
+var pais = 'Japao'
+console.log(`Nascendo no(a) ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Você é brasileiro!')
+} else {
+    console.log('Você é estrangeiro!')
+}
---
+++ js-condições/simples.js
@@ -0,0 +1,7 @@
+var vel = 78
+console.log(`A velocidade atingida foi de ${vel} km/h`)
+
+if (vel > 60) {
+    console.log(`Multado!`)
+}
+// condição simples, não "else"
