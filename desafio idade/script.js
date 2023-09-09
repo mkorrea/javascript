96ec29f08a9f895cd4356db082c72d23fcdeb1c6
mkorrea <mathscorrea@gmail.com>
2023-09-09 20:42:49 -0300

    adicionei imagens

--- desafio idade/script.js
+++ desafio idade/script.js
@@ -10,13 +10,30 @@ function verificar() {
         var sexo = document.getElementsByName('sexo')
         var idade = ano - Number(nasc.value)
         var gen = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (sexo[0].checked) {
             gen = 'um homem'
+            if (idade < 16) {
+                img.setAttribute('src', 'imagens/menino.jpg')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'imagens/homem.jpg')
+            } else {
+                img.setAttribute('src', 'imagens/senhor.jpg')
+            }
         } else if (sexo[1].checked) {
             gen = 'uma mulher'
+            if (idade < 16) {
+                img.setAttribute('src', 'imagens/menina.jpg')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'imagens/mulher.jpg')
+            } else {
+                img.setAttribute('src', 'imagens/senhora.jpg')
+            }
         }
 
-
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gen} com ${idade} anos!`
+        res.appendChild(img)
     }
 }
