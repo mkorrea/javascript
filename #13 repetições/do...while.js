3e5e619e231f319bb35256b446658d9896bb5b63
mkorrea <mathscorrea@gmail.com>
2023-09-11 16:16:53 -0300

    aulas de while e do while

---
+++ #13 repetições/do...while.js
@@ -0,0 +1,7 @@
+// DO WHILE
+// Estrutura de repetição com teste lógico no final
+var c = 1
+do {
+    console.log(`do while | passo ${c}`)
+    c++
+} while (c <= 10) 
---
+++ #13 repetições/while.js
@@ -0,0 +1,8 @@
+// WHILE
+// Estrutura de repetição com teste lógico no início
+var c = 1
+while (c <= 5) {
+    console.log(`while | passo ${c}`)
+    c++ // c = c + 1
+}
+
