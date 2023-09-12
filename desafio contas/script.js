01fd50eda6612e67d10e94ee1ffb862026668d9a
mkorrea <mathscorrea@gmail.com>
2023-09-12 19:27:38 -0300

    desafio de contas com passos

---
+++ desafio contas/script.js
@@ -0,0 +1,14 @@
+function contar() {
+    var initxt = document.getElementById('intxt')
+    var ini = Number(initxt.value)
+    var fimtxt = document.getElementById('fimtxt')
+    var fim = Number(fimtxt.value)
+    var pastxt = document.getElementById('pastxt')
+    var pas = Number(pastxt.value)
+    var res = document.getElementById('res')
+    
+    for (ini; ini < fim; + pas) {
+        res.innerHTML = ini
+    }
+
+}
