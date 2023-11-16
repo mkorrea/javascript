425297f330c72da44f623360ee0b4519934aef54
mkorrea <mathscorrea@gmail.com>
2023-11-16 02:43:25 -0300

    alteração de nomes

--- desafio calculadora/script.js
+++ desafio calculadora/script.js
@@ -1,30 +1,31 @@
 let visor = document.getElementById("visor")
 let calc = document.getElementById("calculo")
 let res = document.getElementById("resultado")
-let calc1 = document.getElementById("c1").innerText
-let n1 = Number(calc1)
+
 let sinal = document.getElementById("sinal")
-let calc3 = document.getElementById("c3").innerText
-let n3 = Number(calc3)
+let valor1 = document.getElementById("valor1")
+let n1 = Number(valor1.innerText)
+let valor2 = document.getElementById("valor2")
+let n2 = Number(valor2.innerText)
 
 function zerar() {
-    if (c3.innerHTML != "" || c1.innerHTML != "" || sinal.innerHTML != "" || res.innerHTML != "") {
+    if (valor1.innerHTML != "" || valor2.innerHTML != "" || sinal.innerHTML != "" || res.innerHTML != "") {
         res.innerHTML = ""
-        c3.innerHTML = ""
+        valor1.innerHTML = ""
         sinal.innerHTML = ""
-        c1.innerHTML = ""
+        valor2.innerHTML = ""
     }
 }
 
 function igual() {  
     if (sinal.innerText == "+") {
-        res.innerHTML = Number(n3) + Number(n1)
+        res.innerHTML = Number(n1) + Number(n2)
     } else if (sinal.innerText == "-") {
-        res.innerHTML = Number(n3) - Number(n1)
+        res.innerHTML = Number(n1) - Number(n2)
     } else if (sinal.innerText == "×") {
-        res.innerHTML = Number(n3) * Number(n1)
+        res.innerHTML = Number(n1) * Number(n2)
     } else if (sinal.innerText == "÷") {
-        res.innerHTML = Number(n3) / Number(n1)
+        res.innerHTML = Number(n1) / Number(n2)
     }
 
 
@@ -32,8 +33,8 @@ function igual() {
 function soma() {
     sinal.innerText = "+"
     if (res.innerHTML != "") {
-        c3.innerHTML = Number(n3) + Number(n1)
-        c1.innerHTML = ""
+        valor1.innerHTML = Number(n1) + Number(n2)
+        valor2.innerHTML = ""
     }
 }
 function sub() {
@@ -49,91 +50,91 @@ function div() {
 
 function sete() {
     if (sinal.innerText == "") {
-        c3.innerText += 7
-        n3 += "7"
-    } else {
-        c1.innerText += 7
+        valor1.innerText += 7
         n1 += "7"
+    } else {
+        valor2.innerText += 7
+        n2 += "7"
     }
 }
 function oito() {
     if (sinal.innerText == "") {
-        c3.innerText += 8
-        n3 += "8"
-    } else {
-        c1.innerText += 8
+        valor1.innerText += 8
         n1 += "8"
+    } else {
+        valor2.innerText += 8
+        n2 += "8"
     }
 }
 function nove() {
     if (sinal.innerText == "") {
-        c3.innerText += 9
-        n3 += "9"
-    } else {
-        c1.innerText += 9
+        valor1.innerText += 9
         n1 += "9"
+    } else {
+        valor2.innerText += 9
+        n2 += "9"
     }
 }
 function quatro() {
     if (sinal.innerText == "") {
-        c3.innerText += 4
-        n3 += "4"
-    } else {
-        c1.innerText += 4
+        valor1.innerText += 4
         n1 += "4"
+    } else {
+        valor2.innerText += 4
+        n2 += "4"
     }
 }
 function cinco() {
     if (sinal.innerText == "") {
-        c3.innerText += 5
-        n3 += "5"
-    } else {
-        c1.innerText += 5
+        valor1.innerText += 5
         n1 += "5"
+    } else {
+        valor2.innerText += 5
+        n2 += "5"
     }
 }
 function seis() {
     if (sinal.innerText == "") {
-        c3.innerText += 6
-        n3 += "6"
-    } else {
-        c1.innerText += 6
+        valor1.innerText += 6
         n1 += "6"
+    } else {
+        valor2.innerText += 6
+        n2 += "6"
     }
 }
 function um() {
     if (sinal.innerText == "") {
-        c3.innerText += 1
-        n3 += "1"
-    } else {
-        c1.innerText += 1
+        valor1.innerText += 1
         n1 += "1"
+    } else {
+        valor2.innerText += 1
+        n2 += "1"
     }
 }
 function dois() {
     if (sinal.innerText == "") {
-        c3.innerText += 2
-        n3 += "2"
-    } else {
-        c1.innerText += 2
+        valor1.innerText += 2
         n1 += "2"
+    } else {
+        valor2.innerText += 2
+        n2 += "2"
     }
 }
 function tres() {
     if (sinal.innerText == "") {
-        c3.innerText += 3
-        n3 += "3"
-    } else {
-        c1.innerText += 3
+        valor1.innerText += 3
         n1 += "3"
+    } else {
+        valor2.innerText += 3
+        n2 += "3"
     }
 }
 function zero() {
     if (sinal.innerText == "") {
-        c3.innerText += 0
-        n3 += "0"
-    } else {
-        c1.innerText += 0
+        valor1.innerText += 0
         n1 += "0"
+    } else {
+        valor2.innerText += 0
+        n2 += "0"
     }
 }
