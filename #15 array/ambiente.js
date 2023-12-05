b9b866c488a0b34ffe389eabab718086c15237d5
mkorrea <mathscorrea@gmail.com>
2023-12-04 23:05:39 -0300

    aula array

---
+++ #15 array/ambiente.js
@@ -0,0 +1,7 @@
+let num = [3,6,9,2,5]
+num.push(8)
+
+console.log(`meu vetor é o: ${num}`)
+console.log(`o vetor tem ${num.length} elementos`)
+console.log(`O primero valor é ${num[0]}`)
+console.log(num.sort())
