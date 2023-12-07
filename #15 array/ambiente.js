87aab8dfdfd2a0e3bed311c27eb005f474b2f3b8
mkorrea <mathscorrea@gmail.com>
2023-12-07 20:24:22 -0300

    for in array

--- #15 array/ambiente.js
+++ #15 array/ambiente.js
@@ -1,7 +1,17 @@
-let num = [3,6,9,2,5]
+let num = [3,60,9,200,5]
+//        0  1  2  3  4
+console.log(num.sort())
 num.push(8)
 
 console.log(`meu vetor é o: ${num}`)
 console.log(`o vetor tem ${num.length} elementos`)
+console.log(num[3])
+
+for (let pos = 0; pos < num.length; pos++) {
+    console.log(`a posição é ${pos} e o valor é ${num[pos]}`)
+}
 console.log(`O primero valor é ${num[0]}`)
-console.log(num.sort())
+
+for (let pos in num){
+    console.log(`a posição é ${pos} e o valor é ${num[pos]}`)
+}
